e1e5bf2a619fe02bd558f2ae0346579961fa6b40
johanmendezb <punkeneto@hotmail.com>
2025-07-29 07:12:47 -0500

    analytics added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import type { AppProps } from 'next/app';
+import { GoogleAnalytics } from '@next/third-parties/google';
 import '../styles/global.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <GoogleAnalytics gaId="G-9ZXP5ZNDT1" />
+    </>
+  );
 }
